f7f459e9650bdfac799b234987c51e799af84cd8
tinta2510 <tatrungtin2004@gmail.com>
2025-02-06 15:24:37 +0700

    Done lexer

--- code/src/main/minigo/parser/MiniGo.g4
+++ code/src/main/minigo/parser/MiniGo.g4
@@ -1,3 +1,4 @@
+// Student ID: 2213506
 grammar MiniGo;
 
 @lexer::header {
@@ -24,6 +25,97 @@ options{
 	language=Python3;
 }
 
+// Lexer rules
+NL: '\r'?'\n' -> skip; //skip newlines
+
+WS : [ \t\r\f]+ -> skip ; // skip spaces, tabs
+
+LINE_COMMENT: '//' ~[\r\n]* -> skip;
+
+MULTI_LINE_COMMENT: '/*' ( MULTI_LINE_COMMENT | .)*? '*/' -> skip;
+
+// Keywords
+IF: 'if';
+ELSE: 'else';
+FOR: 'for';
+RETURN: 'return';
+FUNC: 'func';
+TYPE: 'type';
+STRUCT: 'struct';
+INTERFACE: 'interface';
+STRING: 'string';
+INT: 'int';
+FLOAT: 'float';
+BOOLEAN: 'boolean';
+CONST: 'const';
+VAR: 'var';
+CONTINUE: 'continue';
+BREAK: 'break';
+RANGE: 'range';
+NIL: 'nil';
+TRUE: 'true';
+FALSE: 'false';
+
+// Identifiers (go after keywords)
+ID: [A-Za-z_][A-Za-z0-9_]*;
+
+// Operators
+PLUS: '+';
+MINUS: '-';
+STAR: '*';
+SLASH: '/';
+MOD: '%';
+EQUALS: '==';
+NOT_EQUALS: '!=';
+LESS_THAN: '<';
+LESS_THAN_OR_EQUAL: '<=';
+GREATER_THAN: '>';
+GREATER_THAN_OR_EQUAL: '>=';
+AND: '&&';
+OR: '||';
+NOT: '!';
+ASSIGN: '=';
+PLUS_ASSIGN: '+=';
+MINUS_ASSIGN: '-=';
+STAR_ASSIGN: '*=';
+SLASH_ASSIGN: '/=';
+MOD_ASSIGN: '%=';
+DOT: '.';
+
+// Separators
+L_PAREN: '(';
+R_PAREN: ')';
+L_BRACE: '{';
+R_BRACE: '}';
+L_BRACKET: '[';
+R_BRACKET: ']';
+COMMA: ',';
+SEMICOLON: ';';
+
+// Literals
+INT_LIT: DECIMAL_INT | BINARY_INT | OCTAL_INT | HEX_INT;
+fragment DIGIT: [0-9];
+fragment DECIMAL_INT: [1-9]DIGIT* | '0'; 
+fragment BINARY_INT: '0'[Bb][01]+;
+fragment OCTAL_INT: '0'[Oo][0-7]+;
+fragment HEX_INT: '0'[Xx][0-9A-Fa-f]+;
+
+FLOAT_LIT: DECIMAL_INT '.' DIGIT* EXPONENT? 
+        | DECIMAL_INT EXPONENT ;
+fragment EXPONENT: [eE][+-]?DIGIT+;
+
+STRING_LIT: '"' STRING_CHAR* '"' ;
+fragment STRING_CHAR: (~[\n"\\] | '\\' [ntr"\\]);
+
+ERROR_CHAR: . ;
+ILLEGAL_ESCAPE: '"' STRING_CHAR* '\\' ~[ntr"\\] { self.text = self.text[1:]; } ;
+UNCLOSE_STRING: '"' STRING_CHAR* (NL | EOF) {
+    self.text = self.text[1:]
+    while self.text[-1] == '\n' or self.text[-1] == '\r':
+        self.text = self.text[:-1]; 
+}; 
+
+// Parser rules
 program  : decl+ EOF ;
 
 decl: funcdecl | vardecl  ;
@@ -31,13 +123,3 @@ decl: funcdecl | vardecl  ;
 vardecl: 'var' ID 'int' ';' ;
 
 funcdecl: 'func' ID '(' ')' '{' '}' ';' ;
-
-ID: [a-z]+;
-
-NL: '\n' -> skip; //skip newlines
-
-WS : [ \t\r]+ -> skip ; // skip spaces, tabs 
-
-ERROR_CHAR: .;
-ILLEGAL_ESCAPE:.;
-UNCLOSE_STRING:.;
--- code/src/test/LexerSuite.py
+++ code/src/test/LexerSuite.py
@@ -1,18 +1,86 @@
 import unittest
 from TestUtils import TestLexer
+import inspect
 
-class LexerSuite(unittest.TestCase):
-      
+class LexerSuite(unittest.TestCase): 
     def test_lower_identifier(self):
         """test identifiers"""
         self.assertTrue(TestLexer.checkLexeme("abc","abc,<EOF>",101))
-    
+        
     def test_wrong_token(self):
         self.assertTrue(TestLexer.checkLexeme("ab?sVN","ab,ErrorToken ?",102))
+        
     def test_keyword_var(self):
         """test keyword var"""
         self.assertTrue(TestLexer.checkLexeme("var abc int ;","var,abc,int,;,<EOF>",103))
+        
     def test_keyword_func(self):
         """test keyword func"""
         self.assertTrue(TestLexer.checkLexeme("""func abc ( ) ""","""func,abc,(,),<EOF>""",104))
+        
+    def test_string_lit(self):
+        """test string literal"""
+        self.assertTrue(TestLexer.checkLexeme(""" "abc" """,""""abc",<EOF>""",105))
     
+    def test_comment(self):
+        """test comment"""
+        self.assertTrue(TestLexer.checkLexeme("""/* abc */ var""","""var,<EOF>""",106))
+        
+    def test_multi_comment(self):
+        """test multi comment"""
+        self.assertTrue(TestLexer.checkLexeme("""/* comment */ func ()""","""func,(,),<EOF>""",107))
+        
+    def test_nested_comment(self):
+        """test nested comment"""
+        self.assertTrue(TestLexer.checkLexeme("""/* /* comment */ */ abc""","""abc,<EOF>""",108))
+        
+    def test_nested_comment2(self):
+        """test nested comment"""
+        self.assertTrue(TestLexer.checkLexeme("""/* /* comment */ */ abc /* comment2 */""","""abc,<EOF>""",109))
+        
+    def test_nested_comment3(self):
+        """test nested comment"""
+        self.assertTrue(TestLexer.checkLexeme("""/* /* comment */ comment_content /* comment2 */ */ abc""","""abc,<EOF>""",110))
+        
+    def test_nested_comment4(self):
+        """test nested comment"""
+        self.assertTrue(TestLexer.checkLexeme(
+            """/* comment \n
+                  comment2 \n
+                  comment3 \n
+                */  abc // comment3""","""abc,<EOF>""",111))
+    
+    def test_nested_comment5(self):
+        """test nested comment"""
+        self.assertTrue(TestLexer.checkLexeme(
+            """/* a /* b /* c */ b */ a */	""","""<EOF>""",112))
+        
+    def test_nested_comment6(self):
+        """test nested comment"""
+        self.assertTrue(TestLexer.checkLexeme(
+            """/* a //////* b /* c */ b */ a */	""","""<EOF>""",113))
+        
+    def test_string_lit_2(self):
+        """test string literal"""
+        self.assertTrue(TestLexer.checkLexeme(""" "abc\\ndef" """,""""abc\\ndef",<EOF>""",114))
+        
+    def test_illegal_escape(self):
+        """ILLEGAL_ESCAPE"""
+        self.assertTrue(TestLexer.checkLexeme(""" "abcd\ysafda" ""","Illegal escape in string: abcd\\y", 115))
+        
+    def test_error_token(self):
+        self.assertTrue(TestLexer.checkLexeme("abc?xyz","abc,ErrorToken ?", 116))
+        
+    def test_unclosed_string(self):
+        self.assertTrue(TestLexer.checkLexeme(""" "abc\ndef" ""","Unclosed string: abc", 117))
+        
+    def test_unclosed_string_2(self):
+        self.assertTrue(TestLexer.checkLexeme(
+""""abc
+""","Unclosed string: abc", 118))
+        
+    def test_nested_comment7(self):
+        self.assertTrue(TestLexer.checkLexeme("""/*
+        /* a */ /* b */ 
+        // 321231
+        */ if /* */ /* */""", "if,<EOF>", 119))
